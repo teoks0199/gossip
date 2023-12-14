3a4ca1d0edab607a77b7ba1e46c63285bd219cc4
teoks0199 <teokaisheng3@gmail.com>
2023-12-14 17:06:11 +0800

    Add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   root 'home#index'
+  get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
